220ba6f1f2a4fbae590c368a6e12fb1ffb77c433
Akash0420 <menonakash0420@gmail.com>
2024-04-17 23:30:26 +0530

    c1

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "azurerm_storage_account" "example" {
   name                = "storageaccountname"
   location             = "centralindia"
   resource_group_name  = "cloud-shell-storage-centralindia"
-  account_tier         = "Standard_LRS"
+  account_tier         = "Standard"
   account_replication_type = "LRS"
 }
